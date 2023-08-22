3707c911a6a6689e66ec0670e6fbbf3fe53c6b13
John <132560971+Krokuta001@users.noreply.github.com>
2023-08-22 14:13:15 -0400

    final

--- _printf.c
+++ _printf.c
@@ -1,45 +1,50 @@
 #include "main.h"
-/**
- * _printf - is a function that selects the correct function to print.
- * @format: identifier to look for.
- * Return: the length of the string.
- */
-int _printf(const char * const format, ...)
-{
-	convert_match m[] = {
-		{"%s", printf_string}, {"%c", printf_char},
-		{"%%", printf_37},
-		{"%i", printf_int}, {"%d", printf_dec}, {"%r", printf_srev},
-		{"%R", printf_rot13}, {"%b", printf_bin}, {"%u", printf_unsigned},
-		{"%o", printf_oct}, {"%x", printf_hex}, {"%X", printf_HEX},
-		{"%S", printf_exclusive_string}, {"%p", printf_pointer}
-	};
 
-	va_list args;
-	int i = 0, j, len = 0;
+int _printf(const char *format, ...) {
+    va_list args;
+    va_start(args, format);
 
-	va_start(args, format);
-	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-		return (-1);
+    int count = 0; 
 
-Here:
-	while (format[i] != '\0')
-	{
-		j = 13;
-		while (j >= 0)
-		{
-			if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
-			{
-				len += m[j].f(args);
-				i = i + 2;
-				goto Here;
-			}
-			j--;
-		}
-		_putchar(format[i]);
-		len++;
-		i++;
-	}
-	va_end(args);
-	return (len);
+    while (*format) {
+        if (*format == '%') {
+            format++;
+            if (*format == '\0')
+                break;
+
+            if (*format == 'c') {
+                char c = (char) va_arg(args, int);
+                write(1, &c, 1);
+                count++;
+            } else if (*format == 's') {
+                char *s = va_arg(args, char *);
+                int len = 0;
+                while (s[len] != '\0') {
+                    len++;
+                }
+                write(1, s, len);
+                count += len;
+            } else if (*format == '%') {
+                char percent = '%';
+                write(1, &percent, 1);
+                count++;
+            }
+        } else {
+            write(1, format, 1);
+            count++;
+        }
+
+        format++;
+    }
+
+    va_end(args);
+    return count;
+}
+
+int main() {
+    _printf("Character: %c\n", 'A');
+    _printf("String: %s\n", "Hello, World!");
+    _printf("Percentage sign: %%\n");
+
+    return 0;
 }
